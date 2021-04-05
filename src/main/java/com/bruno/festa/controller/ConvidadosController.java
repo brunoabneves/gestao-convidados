0fdc98387ee173b4139a0564b94123670e913813
Bruno <brunoabneves97@gmail.com>
2021-04-05 19:18:05 -0300

    Enviando um objeto do Controller para a View

--- src/main/java/com/bruno/festa/controller/ConvidadosController.java
+++ src/main/java/com/bruno/festa/controller/ConvidadosController.java
@@ -1,13 +1,25 @@
 package com.bruno.festa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bruno.festa.repository.Convidados;
 
 @Controller
 public class ConvidadosController {
+	
+	@Autowired
+	private Convidados convidados;
+	
 	//método que receberá a requisição e retornará o nome da view
 	@GetMapping("/convidados")
-	public String listar() {
-		return "ListaConvidados";
+	public ModelAndView listar() {
+		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+		
+		modelAndView.addObject("convidados", convidados.findAll());
+		
+		return modelAndView;
 	}
 }
