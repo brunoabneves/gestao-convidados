4130a1e23b0a5af210caa75d36d361593aaaa848
Bruno <brunoabneves97@gmail.com>
2021-04-06 11:30:37 -0300

    Listando objetos com o Thymeleaf

--- src/main/java/com/bruno/festa/model/Convidado.java
+++ src/main/java/com/bruno/festa/model/Convidado.java
@@ -2,9 +2,11 @@ package com.bruno.festa.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Entity
 public class Convidado implements Serializable {
 
 	private static final long serialVersionUID = 1L;
