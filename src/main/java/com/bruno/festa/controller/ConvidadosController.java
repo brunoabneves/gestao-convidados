cab55660156579257b8ed3fbf8e4688eca893646
Bruno <brunoabneves97@gmail.com>
2021-04-06 18:23:45 -0300

    Adicionando um convidado

--- src/main/java/com/bruno/festa/controller/ConvidadosController.java
+++ src/main/java/com/bruno/festa/controller/ConvidadosController.java
@@ -3,8 +3,10 @@ package com.bruno.festa.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bruno.festa.model.Convidado;
 import com.bruno.festa.repository.Convidados;
 
 @Controller
@@ -19,7 +21,15 @@ public class ConvidadosController {
 		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
 		
 		modelAndView.addObject("convidados", convidados.findAll());
+		modelAndView.addObject(new Convidado());
 		
 		return modelAndView;
+	}
+	
+	@PostMapping("/convidados")
+	public String salvar(Convidado convidado) {
+		this.convidados.save(convidado);
+		
+		return "redirect:/convidados";
 	}
 }
